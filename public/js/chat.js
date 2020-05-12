2b2ee9cf920b7b71c7007cb1351bd16e156bf3f6
StechAnurag <anuragtech24.7@gmail.com>
2020-05-12 09:12:55 +0530

    location sharing and event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,6 +7,27 @@ document.querySelector('#message-form').addEventListener('submit', e => {
   // const message = document.querySelector('#message').value;
   const message = e.target.elements.message.value; // referencing with name attribute
   if (!message) return alert('Please write some message!');
-  socket.emit('sendMessage', message);
+  // socket.emit('sendMessage', message);
+  // socket.emit('sendMessage', message, () => console.log('delivered: ✔︎'));
+  // socket.emit('sendMessage', message, serverAck => console.log('delivered: ✔︎', serverAck));
+  socket.emit('sendMessage', message, error => {
+    if (error) return alert(error);
+    console.log('delivered: ✔︎');
+  });
   e.target.elements.message.value = '';
+});
+
+document.querySelector('#loc-btn').addEventListener('click', e => {
+  if (!navigator.geolocation) return alert('Geolocation: not supported by your browser');
+
+  navigator.geolocation.getCurrentPosition(position => {
+    socket.emit(
+      'sendLocation',
+      {
+        location: { lat: position.coords.latitude, long: position.coords.longitude },
+        timestamp: position.timestamp
+      },
+      () => console.log('location shared 🎯') // acknowledgement callback
+    );
+  });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const expres = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = expres();
 const server = http.createServer(app);
@@ -17,8 +18,20 @@ io.on('connection', socket => {
 
   socket.broadcast.emit('message', 'A new user joined.');
 
-  socket.on('sendMessage', message => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+
+    if (filter.isProfane(message)) return callback('Profanity is not allowed');
+
     io.emit('message', message);
+    callback();
+    // callback('delivered');
+  });
+
+  socket.on('sendLocation', (data, cb) => {
+    // io.emit('message', `Location: ${data.location.lat}, ${data.location.long}`);
+    io.emit('message', `https://google.com/maps?q=${data.location.lat},${data.location.long}`);
+    cb(); // acknowledgement
   });
 
   socket.on('disconnect', () => {
